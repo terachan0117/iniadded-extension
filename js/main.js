da13819c785d832803e436367c5d051131a47565
Issei Terada <s1f101803736@iniad.org>
2020-04-29 12:05:33 +0900

    Delete event

--- js/main.js
+++ js/main.js
@@ -10,98 +10,94 @@ function getSearch() {
     return location.search
 }
 
-// DOMツリー構築完了後実行
-$(document).ready(function () {
+// ホストネームを取得し処理を分岐
+switch (getHostname()) {
 
-    // ホストネームを取得し処理を分岐
-    switch (getHostname()) {
+    // INIAD MOOCs
+    case "moocs.iniad.org":
 
-        // INIAD MOOCs
-        case "moocs.iniad.org":
+        // コースページ
+        if (getPathname().match(/^\/courses\/?(\d{4})?\/?$/)) {
 
-            // コースページ
-            if (getPathname().match(/^\/courses\/?(\d{4})?\/?$/)) {
+            // クエリーパラメーターを取得し処理を分岐
+            switch (getSearch()) {
 
-                // クエリーパラメーターを取得し処理を分岐
-                switch (getSearch()) {
+                // ストレージクリア (デバック用)
+                case "?storageclear":
+                    if (window.confirm("ローカルストレージの内容をすべて削除します。この操作は元に戻せません。本当によろしいですか？\n" +
+                            "Delete all the contents of the local storage. This operation cannot be undone. Are you really sure?")) {
+                        localStorage.clear();
+                    }
+                    location.href = "../courses";
+                    break;
 
-                    // ストレージクリア (デバック用)
-                    case "?storageclear":
-                        if (window.confirm("ローカルストレージの内容をすべて削除します。この操作は元に戻せません。本当によろしいですか？\n" +
-                                "Delete all the contents of the local storage. This operation cannot be undone. Are you really sure?")) {
-                            localStorage.clear();
-                        }
-                        location.href = "../courses";
-                        break;
+                default:
 
-                    default:
+                    // コース年度 取得
+                    const CURRENT_YEAR = $(".breadcrumb").text().substring(1);
 
-                        // コース年度 取得
-                        const CURRENT_YEAR = $(".breadcrumb").text().substring(1);
+                    // New Lectures ボタン表示
+                    $('<a class="btn btn-primary btn-sm" href="?newlectures"><i class="fa fa-bell"></i> New Lectures <span id="newlectures_badge" class="badge text-primary"></span></a>')
+                        .insertBefore(".breadcrumb");
 
-                        // New Lectures ボタン表示
-                        $('<a class="btn btn-primary btn-sm" href="?newlectures"><i class="fa fa-bell"></i> New Lectures <span id="newlectures_badge" class="badge text-primary"></span></a>')
-                            .insertBefore(".breadcrumb");
+                    // My Courses ボタン表示
+                    $('<a class="btn btn-default btn-sm" href="?mycourses"><i class="fa fa-book"></i> My Courses</a>')
+                        .insertBefore(".breadcrumb");
 
-                        // My Courses ボタン表示
-                        $('<a class="btn btn-default btn-sm" href="?mycourses"><i class="fa fa-book"></i> My Courses</a>')
-                            .insertBefore(".breadcrumb");
+                    // INIADded バナー表示
+                    $('<div style="position:absolute;width:310px;height:60px;right:0px;top:60px;"><iframe src="https://iniadded.tera-chan.com/baner.html?' +
+                            new Date().getTime() +
+                            '" scrolling="no" style="background-color:transparent;width:100%;height:100%;border:none;"></iframe></div>')
+                        .appendTo("body");
 
-                        // INIADded バナー表示
-                        $('<div style="position:absolute;width:310px;height:60px;right:0px;top:60px;"><iframe src="https://iniadded.tera-chan.com/baner.html?' +
-                                new Date().getTime() +
-                                '" scrolling="no" style="background-color:transparent;width:100%;height:100%;border:none;"></iframe></div>')
-                            .appendTo("body");
-
-                        // Open Drive ボタン表示 (userLang===ja時のみ)
-                        $.ajax({
-                            url: '../account',
-                            cache: false,
-                            datatype: 'html',
-                            success: function (html) {
-                                const userLang = $(html).find('#lang').val();
-                                if (userLang === "ja") {
-                                    $(".well").each(function () {
-                                        let courseName = $(this).find(".media-heading").text();
-                                        courseName = courseName.replace(/[\sIVXABⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩ]/g, "");
-                                        if (courseName.indexOf("（") != -1) {
-                                            courseName = courseName.slice(0, courseName.indexOf("（"));
-                                        }
-                                        if (courseName.indexOf("(") != -1) {
-                                            courseName = courseName.slice(0, courseName.indexOf("("));
-                                        }
-                                        if (courseName.indexOf("＆") != -1) {
-                                            courseName = courseName.slice(0, courseName.indexOf("＆"));
-                                        }
-                                        if (courseName.indexOf("&") != -1) {
-                                            courseName = courseName.slice(0, courseName.indexOf("&"));
-                                        }
-                                        const driveUrl = 'https://drive.google.com/drive/search?q=type:folder%20' +
-                                            CURRENT_YEAR + '%20' +
-                                            encodeURI(courseName) + '%20parent:1KyD2j3o1_IeK7Gum676Ssd0uKDiAybQJ';
-                                        $('<a class="btn btn-default" href="' + driveUrl + '" target="_blank"><i class="fa fa-folder-open"></i> Open Drive</a>')
-                                            .appendTo($(this).find(".media-body"));
-                                    });
-                                }
+                    // Open Drive ボタン表示 (userLang===ja時のみ)
+                    $.ajax({
+                        url: '../account',
+                        cache: false,
+                        datatype: 'html',
+                        success: function (html) {
+                            const userLang = $(html).find('#lang').val();
+                            if (userLang === "ja") {
+                                $(".well").each(function () {
+                                    let courseName = $(this).find(".media-heading").text();
+                                    courseName = courseName.replace(/[\sIVXABⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩ]/g, "");
+                                    if (courseName.indexOf("（") != -1) {
+                                        courseName = courseName.slice(0, courseName.indexOf("（"));
+                                    }
+                                    if (courseName.indexOf("(") != -1) {
+                                        courseName = courseName.slice(0, courseName.indexOf("("));
+                                    }
+                                    if (courseName.indexOf("＆") != -1) {
+                                        courseName = courseName.slice(0, courseName.indexOf("＆"));
+                                    }
+                                    if (courseName.indexOf("&") != -1) {
+                                        courseName = courseName.slice(0, courseName.indexOf("&"));
+                                    }
+                                    const driveUrl = 'https://drive.google.com/drive/search?q=type:folder%20' +
+                                        CURRENT_YEAR + '%20' +
+                                        encodeURI(courseName) + '%20parent:1KyD2j3o1_IeK7Gum676Ssd0uKDiAybQJ';
+                                    $('<a class="btn btn-default" href="' + driveUrl + '" target="_blank"><i class="fa fa-folder-open"></i> Open Drive</a>')
+                                        .appendTo($(this).find(".media-body"));
+                                });
                             }
+                        }
+                    });
+
+                    /* サイドバー */
+
+                    // INIADdedヘッダー 追加
+                    $('<li class="header">INIADded</li>')
+                        .appendTo(".sidebar-menu");
+
+                    // Python REPL ボタン 追加
+                    $('<li><a style="cursor:pointer;"><i class="fa fa-terminal sidebar-shortcut-icon"></i><span> <span class="sidebar-menu-text">Python REPL</span></span></a></li>')
+                        .appendTo(".sidebar-menu").on('click', function () {
+                            openPythonRepl();
                         });
 
-                        /* サイドバー */
-
-                        // INIADdedヘッダー 追加
-                        $('<li class="header">INIADded</li>')
-                            .appendTo(".sidebar-menu");
-                        
-                        // Python REPL ボタン 追加
-                        $('<li><a style="cursor:pointer;"><i class="fa fa-terminal sidebar-shortcut-icon"></i><span> <span class="sidebar-menu-text">Python REPL</span></span></a></li>')
-                            .appendTo(".sidebar-menu").on('click', function () {
-                                openPythonRepl();
-                            });
-
-                }
             }
-            break;
+        }
+        break;
 
-        default:
-    }
-});
+    default:
+}
