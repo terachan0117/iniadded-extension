9cdbc2f7e31d5e5a608329dd4bcb51472003aaa2
Issei Terada <s1f101803736@iniad.org>
2020-04-29 18:05:06 +0900

    Complete

---
+++ js/countCharacter.js
@@ -0,0 +1,17 @@
+function countCharacters(text) {
+    return text.replace(/[\n\s　]/g, '').length;
+}
+
+function countWords(text) {
+    return (text.match(/\S+/g) || []).length;
+}
+
+$(window).on('load', function () {
+    $("textarea").not(".stickynote-textarea").each(function () {
+        $('<p class="text-muted">' + countCharacters($(this).val()) + ' characters ' + countWords($(this).val()) + ' words</p>')
+            .insertBefore(this);
+        $(this).on('input', function () {
+            $(this).prev().text(countCharacters($(this).val()) + ' characters ' + countWords($(this).val()) + ' words');
+        });
+    })
+});
